c8d1461c0ee5a1973ebaf0fde47c17214674ec38
jordandubreuil <jdubreuil@neit.edu>
2025-01-30 10:09:27 -0500

    End of class Week 4

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
+const User = require("./models/User");
+const Person = require("./models/Person");
+const { register } = require("module");
 
 const app = express();
 const port = process.env.port||3000;
@@ -24,9 +27,9 @@ app.use(session({
 }));
 
 //Create a fake user in our database
-const user = {
-    admin:bcrypt.hashSync("12345", 10)
-}
+// const user = {
+//     admin:bcrypt.hashSync("12345", 10)
+// }
 
 function isAuthenticated(req,res, next){
     if(req.session.user)return next();
@@ -46,17 +49,34 @@ db.once("open", ()=>{
     console.log("Connected to MongoDB Database");
 });
 
-//Setup Mongoose Schema
-const peopleSchema = new mongoose.Schema({
-    firstname:String,
-    lastname:String,
-    email:String
-});
 
-const Person = mongoose.model("Person", peopleSchema, "peopledata");
   
 //App Routes
 
+app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+})
+
+app.post("/register", async (req,res)=>{
+    try{
+        const {username, password, email} = req.body;
+
+        const existingUser = await User.findOne({username});
+
+        if(existingUser){
+            return res.send("Username already taken. Try a different one")
+        }
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = new User({username, password:hashedPassword, email});
+        await newUser.save();
+
+        res.redirect("/login");
+
+    }catch(err){
+        res.status(500).send("Error registering new user.");
+    }
+});
 
 app.get("/",(req,res)=>{
     res.sendFile("index.html");
@@ -66,7 +86,7 @@ app.get("/users",isAuthenticated, (req,res)=>{
     res.sendFile(path.join(__dirname, "public", "users.html"));
 });
 
-app.get("/login", (req,res)=>{
+app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname  + "/public/login.html"));
 })
 
@@ -101,22 +121,31 @@ app.post("/addperson", async (req, res)=>{
         const newPerson = new Person(req.body);
         const savePerson = await newPerson.save();
         //res.status(201).json(savePerson);
-        res.redirect("/");
+        res.redirect("/users");
         console.log(savePerson);
     }catch(err){
         res.status(501).json({error:"Failed to add new person."});
     }
 });
 
-app.post("/login", (req,res)=>{
+app.post("/login", async (req,res)=>{
     const {username, password} = req.body;
     console.log(req.body);
-    if(user[username] && bcrypt.compareSync(password, user[username])){
+
+    const user = await User.findOne({username});
+
+    if(user && bcrypt.compareSync(password, user.password)){
         req.session.user = username;
         return res.redirect("/users");
     }
     req.session.error = "Invalid User";
     return res.redirect("/login")
+});
+
+app.get("/logout", (req,res)=>{
+    req.session.destroy(()=>{
+        res.redirect("/login");
+    })
 });
 
 //Update Route
